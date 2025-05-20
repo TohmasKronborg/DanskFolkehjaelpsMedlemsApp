e4f3b3464e1b1d239828b77c85ec5638f0a9ef0e
noahv <non002@edu.zealand.dk>
2025-05-20 09:13:43 +0200

    Base for pramiepuljen.php finished

--- praemiepuljen.php
+++ praemiepuljen.php
@@ -23,7 +23,7 @@ require "settings/init.php";
 
 <body>
 
-<div class="container d-flex justify-content-center flex-column">
+<div class="container d-flex justify-content-center flex-column"> <!-- gør dig klar til at se noget ækelt kode (som jeg helt klart kunne have lavet på en bedre måde) -->
     <div class="row mt-3 mb-3">
         <div class="col d-flex justify-content-center">
             <p class="instrument fw-bold h1">Præmiepuljen</p>
@@ -36,12 +36,55 @@ require "settings/init.php";
             </div>
         </div>
     </div>
+    <div class="row d-flex justify-content-center flex-row">
+        <div class="col d-flex justify-content-center">
+            <p class="instrument h3">Krydstogt til Oslo</p>
+        </div>
+    </div>
     <div class="row mt-3 d-flex justify-content-center flex-row">
         <div class="col d-flex justify-content-center">
-            <div id="countdown" class="text-darkRed fw-bolder h3"></div>
+            <div id="countdown" class="text-darkRed fw-bolder h2"></div>
         </div>
-        <div class="col"
         <hr>
+    </div>
+    <div
+</div>
+
+<div class="container-fluid d-flex justify-content-center">
+    <div class="row d-flex flex-column">
+        <div class="col-12">
+            <p class="text-gray">100 point = 1 billet</p>
+        </div>
+        <div class="col-12 d-flex justify-content-center">
+            <p class="h4">Dit billet antal:</p>
+        </div>
+        <div class="col-12 d-flex justify-content-center">
+            <p class="h4">47</p> <!-- tallet skal erstattes med noget php halløjsa når databasen er sat op -->
+        </div>
+    </div>
+</div>
+
+<hr>
+
+<div class="container-fluid d-flex justify-content-center">
+    <div class="row d-flex flex-column">
+        <div class="col-12 d-flex justify-content-center">
+            <p class="h4 fw-bolder">Køb flere billetter</p>
+        </div>
+        <div class="col-12 d-flex justify-content-center">
+            <input class="form-control border-gray" type="text" placeholder="Antal" aria-label="antal billetter">
+        </div>
+        <div class="col-12 d-flex justify-content-center mt-2">
+            <div class="d-grid gap-2 w-100">
+                <button class="btn btn-primary" type="button">
+                    <p class="h4 d-flex mb-1 mt-1 justify-content-center">Køb</p>
+                </button>
+            </div>
+        </div>
+        <div class="col-12 d-flex justify-content-center mt-3">
+            <p class="text-gray h4">Pris: </p>
+            <p class="text-gray h4">22</p> <!-- Igen, når databasen er sat op, så kan vi lave noget php her der udregner prisen. -->
+        </div>
     </div>
 </div>
 
@@ -49,7 +92,6 @@ require "settings/init.php";
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <script>
-    // Set your target date/time here
     const countdownDate = new Date("2025-07-11T23:59:59").getTime();
 
     const timer = setInterval(() => {
@@ -58,7 +100,7 @@ require "settings/init.php";
 
         if (distance < 0) {
             clearInterval(timer);
-            document.getElementById("countdown").innerHTML = "Countdown expired";
+            document.getElementById("countdown").innerHTML = "En vinder er blevet trukket!";
             return;
         }
 
